da6aa376492ab5d469a8dcc4194e8919037e55b8
Simon Jamain <jamain.simon@gmail.com>
2016-10-26 18:31:01 +0200

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # designexporter-plugin-multipage
 a plugin for designexporter that automate multipage inkscape svg export
+
+it require the utility ```pdfunite``` which is part of ```poppler-utils``` apt package
---
+++ multipage.rb
@@ -0,0 +1,73 @@
+require 'nokogiri'
+require 'fileutils'
+require 'shellwords'
+require 'tempfile'
+require 'byebug'
+
+def multipage (sourceFileName, outputFileName)
+  #load source file
+  svgSource = Nokogiri::XML(File.read(sourceFileName))
+
+  svgAllLayers = svgSource.xpath('//svg:g[@inkscape:groupmode="layer"]')
+
+  tmpPdfFiles = []
+  pageNumber = 1
+  arePagesLeft = true
+
+  while arePagesLeft do
+    #copy svg temporarly
+    svgToEdit = svgSource.clone
+
+    layerName = "Page #{pageNumber}"
+
+    #delete all layers that doesn't match
+    #in detail : select all <g> that are inkscape layers (inkscape:groupmode="layer" attribute) and which doesn't have their layer name (inkscape:label attribute) beginning with the current page number.
+    svgToEdit.xpath('//svg:g[@inkscape:groupmode="layer" and not(starts-with(@inkscape:label,"Page ' + pageNumber.to_s + '"))]').remove
+
+    #only if there is still pages left
+    unless svgToEdit.xpath('//svg:g[@inkscape:groupmode="layer"]').empty? then
+
+      #force layer display
+      svgToEdit.xpath('//svg:g[@inkscape:groupmode="layer"]').each do |svgLayer|
+          svgLayer.attributes['style'].value = 'display:inline'
+      end
+
+      #give a name for temporary file
+      tmpFileNameWithoutExt = "tmp_#{layerName}"
+
+      # create a temporary svg file
+      tmpSourceFile = Tempfile.new([tmpFileNameWithoutExt, '.svg'])
+      File.write(tmpSourceFile.path, svgToEdit.to_xml)
+
+      # create a temporary pdf file
+      tmpPdfFile = Tempfile.new([tmpFileNameWithoutExt, '.pdf'])
+      tmpPdfFiles.push(tmpPdfFile)
+
+      #convert svg to pdf
+      print "#{tmpPdfFile.path} produced.\n" if system "\ninkscape -C --export-pdf=#{tmpPdfFile.path.shellescape} --without-gui #{tmpSourceFile.path.shellescape}\n"
+
+      #remove modified svg
+      tmpSourceFile.close(true)
+
+      #increment page number
+      pageNumber += 1
+    else
+      arePagesLeft = false
+    end
+  end
+
+  if pageNumber > 1
+    # merge pdfs and remove them
+    pdfShellList = tmpPdfFiles.map { |file| file.path.shellescape }.join(" ")
+
+    # ensure path exists
+    FileUtils.mkpath(File.dirname(outputFileName))
+
+    print "#{outputFileName} produced.\n" if system "pdfunite #{pdfShellList} #{outputFileName.shellescape}"
+
+    tmpPdfFiles.each { |tmpPdfFile| tmpPdfFile.close(true) }
+
+    print "temporary files cleaned\n"
+  end
+
+end
